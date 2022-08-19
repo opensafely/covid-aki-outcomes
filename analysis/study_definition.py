487aef093c5c708fbdcc0dd7337c0030d7320c9e
ms576k <michael.sullivan@glasgow.ac.uk>
2022-08-19 10:43:41 +0000

    Example study definition

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,13 +1,28 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
-
+from cohortextractor import (
+    StudyDefinition,
+    codelist,
+    codelist_from_csv,
+    combine_codelists,
+    filter_codes_by_category,
+    patients,
+)
 
 study = StudyDefinition(
+    # define default dummy data behaviour
     default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "date": {"earliest": "1970-01-01", "latest": "today"},
         "rate": "uniform",
-        "incidence": 0.5,
+        "incidence": 0.2,
     },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
-    ),
+
+    # define the study index date
+    index_date="2020-01-01",
+
+    # define the study population
+    population=patients.all(),
+
+    # define the study variables
+    age=patients.age_as_of("index_date")
+
+    # more variables ...
 )
